8115e3ccba5009433cc66eac07f251c048369d53
jimo319 <62179466+jimo319@users.noreply.github.com>
2023-09-08 12:27:16 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,8 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+//let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';//3K大佬的
+let userID = 'b6c50cba-ed21-4f9a-a59b-5895ba22dd7f';//我v2rayN客户端随机生成的
 
 //const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org']; //3K大佬的
 const proxyIPs = ['140.238.10.178', '193.122.103.122', 'icook.hk', 'sp.kjkkk.link', '104.16.60.79', '146.56.152.36', '146.56.154.121', '132.145.87.158', '146.56.176.255', 'cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org', '158.178.246.52', '152.69.221.98', '158.178.229.189', '158.178.229.68'];
